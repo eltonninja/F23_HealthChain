f8f42d08ed4b0846f5d0800a5193c2b770b0c4e7
eltonninja <eltonninja411@gmail.com>
2023-11-27 12:01:05 -0500

    Added doctor and patient user models

--- UI/pages/models.py
+++ UI/pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+#doctor and patient user types
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    specialization = models.CharField(max_length=255)
+
+class Patient(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+
