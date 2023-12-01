f5a969fdd16f11badff1f639fbe0aa314ea936de
eltonninja <eltonninja411@gmail.com>
2023-12-01 11:01:42 -0500

    Added doctor and patient to admin portal

--- UI/accounts/admin.py
+++ UI/accounts/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Doctor, Patient
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
@@ -12,3 +12,21 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['email', 'username',]
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+class DoctorAdmin(admin.ModelAdmin):
+    model = Doctor
+    list_display = ['name', 'phone', 'specialty', 'address', 'city', 'country']
+
+    #change verbose name
+    verbose_name_plural = "doctors"
+
+admin.site.register(Doctor, DoctorAdmin)
+
+class PatientAdmin(admin.ModelAdmin):
+    model = Patient
+    list_display = ['name', 'phone', 'address', 'city', 'country']
+
+    #change verbose name
+    verbose_name_plural = "patients"
+
+admin.site.register(Patient, PatientAdmin)
