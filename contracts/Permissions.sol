82a4f1925542546e8f8188b3dc734908ccd21ce8
Gbodson <110622718+Gbodson@users.noreply.github.com>
2023-12-03 15:15:47 -0500

    changes to smart contract

--- contracts/Permissions.sol
+++ contracts/Permissions.sol
@@ -4,23 +4,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract Permissions {
 
-contract Permissions is ChainlinkClient, Ownable {
+    address private _owner;
+
+    constructor() {
+      _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Not owner");
+        _;
+    }
+
     // Define a struct to hold doctor information
     struct Doctor {
         string name;
         string specialization;
         uint256 rating;
-        bool isAuthorized;
+        //bool isAuthorized;
     }
 
     // Define a struct to hold patient information
     struct Patient {
         string name;
         uint256 birthYear;
-        address[] authorizedDoctors;
+        mapping(address => bool) authorizedDoctors;
+        string hash;
+        string pointer;
     }
 
     // Mapping from doctor addresses to their information
@@ -29,60 +40,63 @@ contract Permissions is ChainlinkClient, Ownable {
     // Mapping from patient addresses to their information
     mapping(address => Patient) public patients;
 
-    // Events to emit on changes
-    event DoctorAdded(address indexed doctorAddress, string name, string specialization);
-    event DoctorAuthorized(address indexed patientAddress, address indexed doctorAddress);
-    event DoctorRevoked(address indexed patientAddress, address indexed doctorAddress);
-    event PatientAdded(address indexed patientAddress, string name);
-
     // Function to add a new doctor
     function addDoctor(address _doctorAddress, string memory _name, string memory _specialization, uint256 _rating) public onlyOwner {
         doctors[_doctorAddress] = Doctor({
             name: _name,
             specialization: _specialization,
-            rating: _rating,
-            isAuthorized: false // Initially, no doctor is authorized to access any patient data
+            rating: _rating
         });
-        emit DoctorAdded(_doctorAddress, _name, _specialization);
     }
 
     // Function for a patient to authorize a doctor
-    function authorizeDoctor(address _doctorAddress) public {
-        require(doctors[_doctorAddress].isAuthorized == false, "Doctor is already authorized.");
-        patients[msg.sender].authorizedDoctors.push(_doctorAddress);
-        doctors[_doctorAddress].isAuthorized = true;
-        emit DoctorAuthorized(msg.sender, _doctorAddress);
+    function authorizeDoctor(address _patientAddress, address _doctorAddress) public onlyOwner {
+        require(!patients[_patientAddress].authorizedDoctors[_doctorAddress], "Doctor is already authorized.");
+        patients[_patientAddress].authorizedDoctors[_doctorAddress] = true;
     }
 
     // Function for a patient to revoke authorization of a doctor
-    function revokeDoctor(address _doctorAddress) public {
-        require(doctors[_doctorAddress].isAuthorized == true, "Doctor is not authorized.");
-        // Logic to remove the doctor's address from the patient's authorizedDoctors array
-        // ...
+    function revokeDoctor(address _patientAddress, address _doctorAddress) public onlyOwner {
+        require(patients[_patientAddress].authorizedDoctors[_doctorAddress], "Doctor is not authorized.");
 
-        doctors[_doctorAddress].isAuthorized = false;
-        emit DoctorRevoked(msg.sender, _doctorAddress);
-    }
+        // Logic to remove the doctor's address from the patient's authorizedDoctors array       
+        patients[_patientAddress].authorizedDoctors[_doctorAddress] = false;      
+      }
 
     // Function to add a new patient
-    function addPatient(address _patientAddress, string memory _name, uint256 _birthYear) public {
-        patients[_patientAddress] = Patient({
-            name: _name,
-            birthYear: _birthYear,
-            authorizedDoctors: new address[](0)
-        });
-        emit PatientAdded(_patientAddress, _name);
-    }
-
-    // Further functions to interact with AI models and oracles will be added here
-    // ...
-
-    // Function to withdraw LINK tokens from the contract (for the Chainlink node operations)
-    function withdrawLink() public onlyOwner {
-        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
-        require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
+    function addPatient(address _patientAddress, string memory _name, uint256 _birthYear) public onlyOwner {
+        //can't use patients[_patientAddress] = (_name, _birthYear) because of the mapping
+        Patient storage temp = patients[_patientAddress];
+        temp.name = _name;
+        temp.birthYear = _birthYear;
     }
 
     // Additional functions for contract management and data handling would be added below
-    // ...
+
+    function returnHash(address _patientAddress) public view returns (string memory) {
+      require(bytes(patients[_patientAddress].hash).length != 0, "Hash not initialized");
+      return patients[_patientAddress].hash;
+    }
+
+    function returnPointer(address _patientAddress) public view returns (string memory) {
+      require(bytes(patients[_patientAddress].pointer).length != 0, "Pointer not initialized");
+      return patients[_patientAddress].pointer;
+    }
+
+    function doctorCheckPermissions(address _patientAddress, address _doctorAddress) public view returns (bool) {
+      if (patients[_patientAddress].authorizedDoctors[_doctorAddress]) {
+        return true;
+      }
+      return false;
+    }
+
+    function setPatientHashPointer(address _patientAddress, string memory _hash, string memory _pointer) public onlyOwner {
+      require(patients[_patientAddress].birthYear != 0, "Patient not initialized");
+      patients[_patientAddress].hash = _hash;
+      patients[_patientAddress].pointer = _pointer;
+    }
+
+    function quickTest(address _patientAddress, address _doctorAddress) public view returns(bool) {
+      return patients[_patientAddress].authorizedDoctors[_doctorAddress];
+    }
 }
